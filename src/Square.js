765486a0840ff9702d6835786e287c0233fee386
Yinka <asyinka@gmail.com>
2023-04-21 05:35:10 +0100

    chore: add basic project elements...

---
+++ src/Square.js
@@ -0,0 +1,17 @@
+const Board = () => {
+  return (
+    <div className="buttons-wrapper">
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+      <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+      <button className="square">7</button>
+      <button className="square">8</button>
+      <button className="square">9</button>
+    </div>
+  );
+};
+
+export default Board;
