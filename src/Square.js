0e391a4aa3a1d988e501fa3942bc0440870426f8
Yinka <asyinka@gmail.com>
2023-04-21 06:38:53 +0100

    test: add a test console.log

--- src/Square.js
+++ src/Square.js
@@ -1,5 +1,13 @@
 const Square = ({ value }) => {
-  return <button className="square">{value}</button>;
+  function handleClick() {
+    console.log("clicked");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
