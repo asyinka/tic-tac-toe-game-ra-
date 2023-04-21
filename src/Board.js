afd52be4cac4c96aba76a4a48eb90a67592ea98b
Yinka <asyinka@gmail.com>
2023-04-21 07:29:23 +0100

    chore: add useState variables

--- src/Board.js
+++ src/Board.js
@@ -3,15 +3,15 @@ import Square from "./Square";
 const Board = () => {
   return (
     <div className="buttons-wrapper">
-      <Square value={1} />
-      <Square value={2} />
-      <Square value={3} />
-      <Square value={4} />
-      <Square value={5} />
-      <Square value={6} />
-      <Square value={7} />
-      <Square value={8} />
-      <Square value={9} />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
     </div>
   );
 };
