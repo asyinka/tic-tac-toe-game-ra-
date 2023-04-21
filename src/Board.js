042fe088267a2cdd65aea3d854314bd2820ccd95
Yinka <asyinka@gmail.com>
2023-04-21 11:14:09 +0100

    chore: add conditional switch

--- src/Board.js
+++ src/Board.js
@@ -2,28 +2,35 @@ import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [xIsNext, setXisNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   //this holds the state of the squares and update only the first colum to 'X'
-  function handleClick() {
+  function handleClick(i) {
     const nextSquare = squares.slice();
 
-    nextSquare[0] = "X";
-    setSquares(nextSquare[0]);
+    if (xIsNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+
+    setSquares(nextSquare);
+    setXisNext(!xIsNext);
   }
 
   squares.slice();
 
   return (
     <div className="buttons-wrapper">
-      <Square value={squares[0]} onSquareClick={handleClick} />
-      <Square value={squares[1]} onSquareClick={handleClick} />
-      <Square value={squares[2]} onSquareClick={handleClick} />
-      <Square value={squares[3]} onSquareClick={handleClick} />
-      <Square value={squares[4]} onSquareClick={handleClick} />
-      <Square value={squares[5]} onSquareClick={handleClick} />
-      <Square value={squares[6]} onSquareClick={handleClick} />
-      <Square value={squares[7]} onSquareClick={handleClick} />
-      <Square value={squares[8]} onSquareClick={handleClick} />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   );
 };
