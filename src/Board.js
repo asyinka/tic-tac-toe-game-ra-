3f3c4786ac8e9f0911b9bbb549dcc74fb249dc22
Yinka <asyinka@gmail.com>
2023-04-21 09:36:11 +0100

    chore: create and add the handleClick function as prop

--- src/Board.js
+++ src/Board.js
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  //this holds the state of the squares and update only the first colum to 'X'
+  function handleClick() {
+    const nextSquare = squares.slice();
+
+    nextSquare[0] = "X";
+    setSquares(nextSquare[0]);
+  }
+
+  squares.slice();
+
   return (
     <div className="buttons-wrapper">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={handleClick} />
+      <Square value={squares[1]} onSquareClick={handleClick} />
+      <Square value={squares[2]} onSquareClick={handleClick} />
+      <Square value={squares[3]} onSquareClick={handleClick} />
+      <Square value={squares[4]} onSquareClick={handleClick} />
+      <Square value={squares[5]} onSquareClick={handleClick} />
+      <Square value={squares[6]} onSquareClick={handleClick} />
+      <Square value={squares[7]} onSquareClick={handleClick} />
+      <Square value={squares[8]} onSquareClick={handleClick} />
     </div>
   );
 };
