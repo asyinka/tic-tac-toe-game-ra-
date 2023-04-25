4bd76e03cbf95e76305024f94144d90d0d0bb320
Yinka <asyinka@gmail.com>
2023-04-25 14:19:13 +0100

    handlePlay function updates Game’s state to trigger a re-render

--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import Board from "./Board";
 
 const Game = () => {
-  const [xIsNext, setXisNext] = useState(true);
+  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquare = history[history.length - 1];
 
-  function handlePlay(nextSquares) {}
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
 
   return (
     <div className="game">
