13817905b147429b03308c746a94e9c537fcb296
Yinka <asyinka@gmail.com>
2023-04-25 14:15:27 +0100

    add variables to Game component

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Board from "./Board";
 
 const Game = () => {
+  const [xIsNext, setXisNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquare = history[history.length - 1];
+
+  function handlePlay(nextSquares) {}
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol></ol>
       </div>
     </div>
   );
