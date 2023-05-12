2d7ea58d499fccdff5df2dc62dabca0383da08db
Yinka <asyinka@gmail.com>
2023-05-12 14:47:32 +0100

    Chores: each move is shown in list

--- src/Board.js
+++ src/Board.js
@@ -65,7 +65,7 @@ function checkWinner(squares) {
     const [a, b, c] = lines[i];
 
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return `${squares[a]}`;
     }
   }
   return null;
